380fcfd925c8d016fb22caef2342a383fbed6987
sanglap13 <sanglapmridha@gmail.com>
2024-07-16 15:31:31 +0530

    fix: get api working

--- backend/iot/views.py
+++ backend/iot/views.py
@@ -1,35 +1,27 @@
-from rest_framework import generics
+from django.db.models import Avg, Max, Min
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import DeviceData
 from .serializers import DeviceDataSerializer
-from django.db.models import Avg, Min, Max
 
-class DeviceDataCreateView(generics.CreateAPIView):
-    queryset = DeviceData.objects.all()
+
+class DeviceDataAnalyticsView(APIView):
     serializer_class = DeviceDataSerializer
 
-class DeviceDataAnalyticsView(generics.ListAPIView):
-    serializer_class = DeviceDataSerializer
-
-    def get_queryset(self):
-        device_id = self.request.query_params.get('device_id')
-        start_time = self.request.query_params.get('start_time')
-        end_time = self.request.query_params.get('end_time')
+    def get(self, request):
+        queryset = DeviceData.objects.all()
+        forattedData = DeviceDataSerializer(queryset, many=True).data
+        print(forattedData)
         
-        queryset = DeviceData.objects.filter(device_id=device_id, timestamp__range=[start_time, end_time])
-        return queryset
-
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-        data = response.data
+        avg_value = queryset.aggregate(Avg('sensor_value'))['sensor_value__avg']
+        min_value = queryset.aggregate(Min('sensor_value'))['sensor_value__min']
+        max_value = queryset.aggregate(Max('sensor_value'))['sensor_value__max']
         
-        avg_value = self.get_queryset().aggregate(Avg('sensor_value'))['sensor_value__avg']
-        min_value = self.get_queryset().aggregate(Min('sensor_value'))['sensor_value__min']
-        max_value = self.get_queryset().aggregate(Max('sensor_value'))['sensor_value__max']
-        
-        response.data = {
-            'data': data,
-            'average': avg_value,
-            'minimum': min_value,
-            'maximum': max_value
+        response = {
+            'data': forattedData,
+            'avg_value': avg_value,
+            'min_value': min_value,
+            'max_value': max_value
         }
-        return response
+        return Response(response)
