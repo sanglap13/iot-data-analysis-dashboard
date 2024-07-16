5e3a0acd4aa5fccd9e612198d0eaad4aab638a5b
sanglap13 <sanglapmridha@gmail.com>
2024-07-16 21:04:17 +0530

    refactor: add error handling in get api

--- backend/iot/views.py
+++ backend/iot/views.py
@@ -28,38 +28,50 @@ class DeviceDataCreateView(APIView):
 class DeviceDataAnalyticsView(APIView):
 
     def get(self, request):
-        queryset = DeviceData.objects.all()
-        formattedData = DeviceDataSerializer(queryset, many=True).data
-        
-        avg_temperature = queryset.aggregate(Avg('temperature'))['temperature__avg']
-        min_temperature = queryset.aggregate(Min('temperature'))['temperature__min']
-        max_temperature = queryset.aggregate(Max('temperature'))['temperature__max']
+        try:
+            queryset = DeviceData.objects.all()
+            formattedData = DeviceDataSerializer(queryset, many=True).data
+            
+            avg_temperature = queryset.aggregate(Avg('temperature'))['temperature__avg']
+            min_temperature = queryset.aggregate(Min('temperature'))['temperature__min']
+            max_temperature = queryset.aggregate(Max('temperature'))['temperature__max']
 
-        avg_humidity = queryset.aggregate(Avg('humidity'))['humidity__avg']
-        min_humidity = queryset.aggregate(Min('humidity'))['humidity__min']
-        max_humidity = queryset.aggregate(Max('humidity'))['humidity__max']
+            avg_humidity = queryset.aggregate(Avg('humidity'))['humidity__avg']
+            min_humidity = queryset.aggregate(Min('humidity'))['humidity__min']
+            max_humidity = queryset.aggregate(Max('humidity'))['humidity__max']
 
-        avg_other = queryset.aggregate(Avg('other'))['other__avg']
-        min_other = queryset.aggregate(Min('other'))['other__min']
-        max_other = queryset.aggregate(Max('other'))['other__max']
+            avg_other = queryset.aggregate(Avg('other'))['other__avg']
+            min_other = queryset.aggregate(Min('other'))['other__min']
+            max_other = queryset.aggregate(Max('other'))['other__max']
+            
+            response = {
+                    'data': {
+                        'device_data': formattedData,
+                        'temperature': {
+                            'avg': avg_temperature,
+                            'min': min_temperature,
+                            'max': max_temperature
+                        },
+                        'humidity': {
+                            'avg': avg_humidity,
+                            'min': min_humidity,
+                            'max': max_humidity
+                        },
+                        'other': {
+                            'avg': avg_other,
+                            'min': min_other,
+                            'max': max_other
+                        }
+                    },
+                    'msg': 'Data retrieved successfully',
+                    'status': True
+                }
+
+            return Response(response, status=status.HTTP_200_OK)
         
-        response = {
-            'data': formattedData,
-            'temperature': {
-                'avg': avg_temperature,
-                'min': min_temperature,
-                'max': max_temperature
-            },
-            'humidity': {
-                'avg': avg_humidity,
-                'min': min_humidity,
-                'max': max_humidity
-            },
-            'other': {
-                'avg': avg_other,
-                'min': min_other,
-                'max': max_other
+        except Exception as e:
+            error_response = {
+                'msg': f'Error retrieving data: {str(e)}',
+                'status': False
             }
-        }
-
-        return Response(response)
+            return Response(error_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
