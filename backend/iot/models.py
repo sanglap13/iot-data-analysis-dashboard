a64dc9f344335cb9b48f13ce8a568aa3d2e33c61
sanglap13 <sanglapmridha@gmail.com>
2024-07-18 22:23:32 +0530

    refactor: change model and serializer to use join

--- backend/iot/models.py
+++ backend/iot/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class DeviceData(models.Model):
-    device_id = models.CharField(max_length=50)
-    timestamp = models.DateTimeField(auto_now_add=True)
+class SensorValue(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
     other = models.FloatField()
 
     def __str__(self):
-        return f"Device {self.device_id} - Temp: {self.temperature}, Humidity: {self.humidity}, Other: {self.other} at {self.timestamp}"
+        return f"Temp: {self.temperature}, Humidity: {self.humidity}, Other: {self.other}"
 
+class DeviceData(models.Model):
+    device_id = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    sensor_value = models.OneToOneField(SensorValue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Device {self.device_id} - {self.sensor_value} at {self.timestamp}"
--- backend/iot/serializers.py
+++ backend/iot/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import DeviceData
+from .models import DeviceData, SensorValue
+
+class SensorValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SensorValue
+        fields = ['temperature', 'humidity', 'other']
 
 class DeviceDataSerializer(serializers.ModelSerializer):
+    sensor_value = SensorValueSerializer()
+
     class Meta:
         model = DeviceData
-        fields = ['device_id', 'timestamp', 'temperature', 'humidity', 'other']
+        fields = ['device_id', 'timestamp', 'sensor_value']
+
+    def create(self, validated_data):
+        sensor_value_data = validated_data.pop('sensor_value')
+        sensor_value = SensorValue.objects.create(**sensor_value_data)
+        device_data = DeviceData.objects.create(sensor_value=sensor_value, **validated_data)
+        return device_data
