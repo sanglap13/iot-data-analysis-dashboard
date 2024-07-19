d83f2cd7afa3765a11d7c73fb6dadcbe69afaad6
sanglap13 <sanglapmridha@gmail.com>
2024-07-19 18:10:29 +0530

    chore: variable name change

--- frontend/src/utils/api/api.ts
+++ frontend/src/utils/api/api.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 import { apiResponse, apiData } from "../../@types/api/api.types";
 
-const API_URL = "http://127.0.0.1:8000/api/iot/analytics/";
+const REST_API_URL = "http://127.0.0.1:8000/api/iot/analytics/";
 
 export const api = async (): Promise<undefined | apiData> => {
   try {
-    const response: AxiosResponse<apiResponse> = await axios.get(API_URL);
+    const response: AxiosResponse<apiResponse> = await axios.get(REST_API_URL);
     if (response.status) {
       const { data } = response.data;
       return data;
