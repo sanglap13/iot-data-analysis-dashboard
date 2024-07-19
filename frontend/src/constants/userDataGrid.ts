36efdee41eda3cd78d92436ed8b490c805165f32
sanglap13 <sanglapmridha@gmail.com>
2024-07-19 18:25:00 +0530

    fix: ui issue

--- frontend/src/constants/userDataGrid.ts
+++ frontend/src/constants/userDataGrid.ts
@@ -1,5 +1,5 @@
 export const DATAGRID_COLUMNS = [
-  { field: "device_id", headerName: "Device ID", flex: 1 },
+  { field: "device_id", headerName: "Device", flex: 1 },
   { field: "temperature", headerName: "Temperature (C)", flex: 1 },
   { field: "humidity", headerName: "Humidity (%)", flex: 1 },
   { field: "other", headerName: "Other", flex: 1 },
